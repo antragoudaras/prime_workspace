04f65364742a81e431a1a2f090c75d47673a9292
antragoudaras <antonistragoudaras@gmail.com>
2022-06-03 16:28:45 +0300

    update launch_train.sh directory

--- launch_train.sh
+++ launch_train.sh
@@ -11,6 +11,6 @@ SRC_DIR="$PROJECT_DIR"
 JOB_RESULTS_DIR="$PROJECT_DIR"/results
 mkdir -p "$JOB_RESULTS_DIR"
 
-JOB_NAME=continue-uNAS-cnn_mnist_struct_pru
+JOB_NAME=train_prime_50000_grad_steps
 
 sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train.sbatch
