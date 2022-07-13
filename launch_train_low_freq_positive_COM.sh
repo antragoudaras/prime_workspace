287c812d66407bdfafda1ca749b3ce0ca13da37e
antragoudaras <antonistragoudaras@gmail.com>
2022-07-13 16:34:08 +0300

    positive COM low freq PRIME run

---
+++ launch_train_low_freq_positive_COM.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+# script should be run from the project directory
+PROJECT_DIR="$PWD"
+
+# project should have a src directory
+SRC_DIR="$PROJECT_DIR"
+JOB_RESULTS_DIR="$PROJECT_DIR"/results_low_freq_positive_COM
+mkdir -p "$JOB_RESULTS_DIR"
+
+JOB_NAME=extented_low_freq_train_prime_60000_grad_steps_batch_size_1000_positive_COM
+
+sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_low_freq_positive_COM.sbatch
