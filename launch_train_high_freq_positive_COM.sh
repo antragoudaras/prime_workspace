0bb084bad41d0b4bb36894eb036c332883b91328
antragoudaras <antonistragoudaras@gmail.com>
2022-07-15 00:13:57 +0300

    rerun high freq PRIME + positive COM

--- launch_train_high_freq_positive_COM.sh
+++ launch_train_high_freq_positive_COM.sh
@@ -8,9 +8,9 @@ PROJECT_DIR="$PWD"
 
 # project should have a src directory
 SRC_DIR="$PROJECT_DIR"
-JOB_RESULTS_DIR="$PROJECT_DIR"/results_high_freq
+JOB_RESULTS_DIR="$PROJECT_DIR"/results_high_freq_positive_COM
 mkdir -p "$JOB_RESULTS_DIR"
 
-JOB_NAME=run2_extented_high_freq_train_prime_60000_grad_steps_batch_size_1000
+JOB_NAME=extented_high_freq_train_prime_60000_grad_steps_batch_size_1000_positive_COM
 
-sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_high_freq.sbatch
+sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_high_freq_positive_COM.sbatch
