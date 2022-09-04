14a765f0649427e4d971fe316535a3f478ee23d1
antragoudaras <antonistragoudaras@gmail.com>
2022-09-04 17:05:57 +0300

    update launch script for negatives

---
+++ launch_train_photonics_negative.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+# script should be run from the project directory
+PROJECT_DIR="$PWD"
+
+# project should have a src directory
+SRC_DIR="$PROJECT_DIR"
+JOB_RESULTS_DIR="$PROJECT_DIR"/results_photonics_negative_worst_80_split
+EXCEL_RESULTS_DIR="$PROJECT_DIR"/photonics_optimized_negative_results_worst_80_split
+
+mkdir -p "$JOB_RESULTS_DIR"
+mkdir -p "$EXCEL_RESULTS_DIR"
+
+JOB_NAME=train_photonics_60000_grad_steps_batch_size_375_worst_80_split
+
+sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_photonics_negative.sbatch
