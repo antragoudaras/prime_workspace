5c32dc697b421a06d6bf0a9e745644745698fbad
antragoudaras <antonistragoudaras@gmail.com>
2022-10-08 19:18:30 +0300

    prime NAS on ECoG dataset

---
+++ launch_train_ECoG_positive_COM_best_split.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# entire script fails if a single command fails
+set -e
+
+# script should be run from the project directory
+PROJECT_DIR="$PWD"
+
+# project should have a src directory
+SRC_DIR="$PROJECT_DIR"
+JOB_RESULTS_DIR="$PROJECT_DIR"/ECoG_positive_COM_best_split_logs
+OPTIMIZED_DATASET_DIR="$PROJECT_DIR"/ECoG_positive_COM_optimized_params
+
+mkdir -p "$JOB_RESULTS_DIR"
+mkdir -p "$OPTIMIZED_DATASET_DIR"
+
+JOB_NAME=train_ECoG_60000_grad_steps_batch_size_1000_positive_COM_best_split
+
+sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_ECoG_positive_COM_best_split.sbatch
