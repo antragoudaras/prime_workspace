6758467c9dadada1fff748ec57c8a921f43c375a
antragoudaras <antonistragoudaras@gmail.com>
2022-10-30 11:35:40 +0300

    run for 120000 grad steps

--- launch_train_photonics_positive_mixed_split.sh
+++ launch_train_photonics_positive_mixed_split.sh
@@ -14,6 +14,6 @@ EXCEL_RESULTS_DIR="$PROJECT_DIR"/october_photonics_optimized_positive_results_mi
 mkdir -p "$JOB_RESULTS_DIR"
 mkdir -p "$EXCEL_RESULTS_DIR"
 
-JOB_NAME=train_photonics_60000_grad_steps_mixed_split_votes_one
+JOB_NAME=train_photonics_120000_grad_steps_mixed_split_votes_one
 
 sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_photonics_positive_mixed_split.sbatch
