5a0c6467805612074f603f07b80265fc101a713a
antragoudaras <antonistragoudaras@gmail.com>
2022-11-01 12:45:24 +0300

    tuning hyperparametrs

--- launch_train_photonics_positive_mixed_split.sh
+++ launch_train_photonics_positive_mixed_split.sh
@@ -14,6 +14,11 @@ EXCEL_RESULTS_DIR="$PROJECT_DIR"/october_photonics_optimized_positive_results_mi
 mkdir -p "$JOB_RESULTS_DIR"
 mkdir -p "$EXCEL_RESULTS_DIR"
 
-JOB_NAME=train_photonics_120000_grad_steps_mixed_split_votes_one
+CQL_ALPHA=5.0
+INFEASIBLE_ALPHA=5.0
+NUM_VOTES=1
+TRAIN_STEPS=120001
 
-sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_photonics_positive_mixed_split.sbatch
+JOB_NAME=PRIME_photonics_${TRAIN_STEPS}_grad_steps_mixed_split_${NUM_VOTES}_votes_${CQL_ALPHA}_cql_alpha_${INFEASIBLE_ALPHA}_infeasible_alpha
+
+sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_photonics_positive_mixed_split.sbatch --cql_alpha ${CQL_ALPHA} --infeasible_alpha ${INFEASIBLE_ALPHA} --num_votes ${NUM_VOTES} --train_steps ${TRAIN_STEPS}
--- prime_photonics_positive_mixed_split.py
+++ prime_photonics_positive_mixed_split.py
@@ -13,8 +13,17 @@ import numpy as np
 import os
 import pickle
 import csv
+import argparse
 from typing import Optional, Dict, List
 from copy import deepcopy
+
+
+parser = argparse.ArgumentParser("hyperparameter Tuning for PRIME")
+parser.add_argument("--cql_alpha", type=float, default=0.1, help="Tune cql_alpha")
+parser.add_argument("--infeasible_alpha", type=float, default=0.05, help="Tune infeasible alpha")
+parser.add_argument("--num_votes", type=int, default=1, help="A search state file to resume from")
+parser.add_argument("--train_steps", type=int, default=120000, help="A search state file to resume from")
+args = parser.parse_args()
 
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')), flush=True)
@@ -1274,7 +1283,7 @@ class FireflyAlg():
           distance = np.absolute(value_range - _remainder)
           arg_min = np.argmin(distance)
           valid_firefly = value_range[arg_min]
-        else:
+        else: #_remainder == 0
           valid_firefly = value_range[-1]
       else:
         distance = np.absolute(value_range - temp_calc)
@@ -1490,7 +1499,7 @@ def train_eval_offline(
     random_dataset['param_2'] = param_2_series.map({64: 0.64, 65: 0.65, 66: 0.66, 67: 0.67, 68: 0.68, 69: 0.69, 70: 0.7, 71: 0.71, 72: 0.72, 73: 0.73, 74: 0.74, 75: 0.75})
     random_dataset['param_4'] = param_4_series.map({100: 1.0, 101: 1.01, 102: 1.02, 103: 1.03, 104: 1.04, 105: 1.05, 106: 1.06, 107: 1.07, 108:1.08, 109: 1.09, 110: 1.1, 111: 1.11})
     # random_dataset.to_csv(f'./photonics_optimized_positive_results_worst_80_split/random_dataset_worst_80_train_split{batch_size}_size.csv')
-    random_dataset.to_excel(f'./october_photonics_optimized_positive_results_mixed_80_split/random_dataset_{num_votes}_votes.xlsx')
+    random_dataset.to_excel(f'./november_photonics_optimized_positive_results_mixed_80_split/random_dataset_{train_data}_steps{num_votes}_votes_{cql_alpha}_cql_alpha_{infeasible_alpha}_infeasible_alpha.xlsx')
 
     print('Start Discerte Optimizer (Metaheuristic (Firelfy) Algorithm) for training_dataset designs')
     discrete_optimizer2 = FireflyAlg(initial_dataset=training_dataset, config=config, population=25, remainder=True, random_fireflies=False)
@@ -1513,7 +1522,7 @@ def train_eval_offline(
     train_dataset['param_2'] = param_2_series.map({64: 0.64, 65: 0.65, 66: 0.66, 67: 0.67, 68: 0.68, 69: 0.69, 70: 0.7, 71: 0.71, 72: 0.72, 73: 0.73, 74: 0.74, 75: 0.75})
     train_dataset['param_4'] = param_4_series.map({100: 1.0, 101: 1.01, 102: 1.02, 103: 1.03, 104: 1.04, 105: 1.05, 106: 1.06, 107: 1.07, 108:1.08, 109: 1.09, 110: 1.1, 111: 1.11})
     # train_dataset.to_csv(f'./photonics_optimized_positive_results_worst_80_split/train_dataset_worst_80_train_split{batch_size}_size.csv')
-    train_dataset.to_excel(f'./october_photonics_optimized_positive_results_mixed_80_split/train_dataset_{num_votes}_votes.xlsx')
+    train_dataset.to_excel(f'./november_photonics_optimized_positive_results_mixed_80_split/train_dataset_{train_data}_steps{num_votes}_votes_{cql_alpha}_cql_alpha_{infeasible_alpha}_infeasible_alpha.xlsx')
     
     print('Start Discerte Optimizer (Metaheuristic (Firelfy) Algorithm) for validation_dataset designs')
     discrete_optimizer3 = FireflyAlg(initial_dataset=validation_dataset, config=config, population=25, remainder=True, random_fireflies=False)
@@ -1537,7 +1546,7 @@ def train_eval_offline(
     val_dataset['param_2'] = param_2_series.map({64: 0.64, 65: 0.65, 66: 0.66, 67: 0.67, 68: 0.68, 69: 0.69, 70: 0.7, 71: 0.71, 72: 0.72, 73: 0.73, 74: 0.74, 75: 0.75})
     val_dataset['param_4'] = param_4_series.map({100: 1.0, 101: 1.01, 102: 1.02, 103: 1.03, 104: 1.04, 105: 1.05, 106: 1.06, 107: 1.07, 108:1.08, 109: 1.09, 110: 1.1, 111: 1.11})
     # val_dataset.to_csv(f'./photonics_optimized_positive_results_worst_80_split/val_dataset_worst_80_train_split{batch_size}_size.csv')
-    val_dataset.to_excel(f'./october_photonics_optimized_positive_results_mixed_80_split/val_dataset_{num_votes}_votes.xlsx')
+    val_dataset.to_excel(f'./november_photonics_optimized_positive_results_mixed_80_split/val_dataset_{train_data}_steps{num_votes}_votes_{cql_alpha}_cql_alpha_{infeasible_alpha}_infeasible_alpha.xlsx')
 
 config_str = """discrete:param_1:float64:true:450,451,452,453,454,455,456,457,458,459,460,461,462,463,464,465,466,467,468,469,470,471,472,473,474,475,476,477,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,517,518,519,520,521,522,523,524,525,526,527,528,529,530,531,532,533,534,535,536,537,538,539,540,541,542,543,544,545,546,547,548,549,550,551,552,553,554,555,556,557,558,559,560,561,562,563,564,565,566,567,568,569,570,571,572,573,574,575,576,577,578,579,580,581,582,583,584,585,586,587,588,589,590,591,592,593,594,595,596,597,598,599,600,601,602,603,604,605,606,607,608,609,610,611,612,613,614,615,616,617,618,619,620,621,622,623,624,625,626,627,628,629,630,631,632,633,634,635,636,637,638,639,640,641,642,643,644,645,646,647,648,649,650,651,652,653,654,655,656,657,658,659,660,661,662,663,664,665,666,667,668,669,670,671,672,673,674,675,676,677,678,679,680,681,682,683,684,685,686,687,688,689,690,691,692,693,694,695,696,697,698,699,700,701,702,703,704,705,706,707,708,709,710,711,712,713,714,715,716,717,718,719,720,721,722,723,724,725,726,727,728,729,730,731,732,733,734,735,736,737,738,739,740,741,742,743,744,745,746,747,748,749,750
 discrete:param_2:float64:true:64,65,66,67,68,69,70,71,72,73,74,75
@@ -1607,20 +1616,20 @@ train_eval_offline(
   config=config_str,
   training_dataset=training_data,
   validation_dataset=validation_data,
-  train_steps=120001,
+  train_steps=args.train_steps,
   summary_freq=100,
-  eval_freq=250,
+  eval_freq=100,
   add_summary=True,
-  save_dir="positive_mixed_split_num_votes_one",
+  save_dir=f"positive_mixed_split_{args.train_setps}_steps_{args.num_votes}_votes_{args.cql_alpha}_cql_alpha_{args.infeasible_alpha}_infeasible_alpha",
   loss_type='mse+rank',
   layers=(256, 256, 256),
   with_ranking_penalty=True,
   ranking_penalty_weight=0.1,
   batch_size=53,
   use_dropout=True,
-  num_votes=1,
-  cql_alpha=0.1,
-  infeasible_alpha=0.05,
+  num_votes=args.num_votes,
+  cql_alpha=args.cql_alpha,
+  infeasible_alpha=args.infeasible_alpha,
   enable_discrete_optimizer=True,
   skip_training=False
 )
