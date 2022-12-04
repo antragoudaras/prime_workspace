7648d71ac309e9655268451fde4eb60382675690
antragoudaras <antonistragoudaras@gmail.com>
2022-12-04 15:12:05 +0300

    60k leave one out contextual PRIME EEG high-freq

--- launch_train_leave_one_out_contextual_prime_NAS_EEG_positive_COM_mixed_split_high_freq.sh
+++ launch_train_leave_one_out_contextual_prime_NAS_EEG_positive_COM_mixed_split_high_freq.sh
@@ -20,6 +20,6 @@ NUM_VOTES=1
 TRAIN_STEPS=30001
 BATCH_SIZE=512
 
-JOB_NAME=leave_one_out_PRIME_contextual_EEG_high_freq_positive_COM_mixed_split_${TRAIN_STEPS}_grad_steps_mixed_split_${NUM_VOTES}_votes_${CQL_ALPHA}_cql_alpha_${INFEASIBLE_ALPHA}_infeasible_alpha
+JOB_NAME=resume_leave_one_out_PRIME_contextual_EEG_high_freq_positive_COM_mixed_split_${TRAIN_STEPS}_grad_steps_mixed_split_${NUM_VOTES}_votes_${CQL_ALPHA}_cql_alpha_${INFEASIBLE_ALPHA}_infeasible_alpha
 
 sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_leave_one_out_contextual_prime_NAS_EEG_positive_COM_mixed_split_high_freq.sbatch --cql_alpha ${CQL_ALPHA} --infeasible_alpha ${INFEASIBLE_ALPHA} --num_votes ${NUM_VOTES} --train_steps ${TRAIN_STEPS} --batch_size ${BATCH_SIZE}
