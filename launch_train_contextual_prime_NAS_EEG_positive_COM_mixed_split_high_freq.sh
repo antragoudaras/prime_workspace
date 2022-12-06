6f28fa4de404e81f31bdb2f8b7699d0eefbbef78
antragoudaras <antonistragoudaras@gmail.com>
2022-12-06 17:12:50 +0300

    added echo command for clarity of job description

--- launch_train_contextual_prime_NAS_EEG_positive_COM_mixed_split_high_freq.sh
+++ launch_train_contextual_prime_NAS_EEG_positive_COM_mixed_split_high_freq.sh
@@ -24,3 +24,5 @@ TOTAL_STEPS=100000
 JOB_NAME=PRIME_contextual_all_subjs_EEG_high_freq_${TOTAL_STEPS}_grad_steps_${NUM_VOTES}_votes_${CQL_ALPHA}_cql_alpha_${INFEASIBLE_ALPHA}_infeasible_alpha
 
 sbatch --job-name "$JOB_NAME" "$SRC_DIR"/train_contextual_prime_NAS_EEG_positive_COM_mixed_split_high_freq.sbatch --cql_alpha ${CQL_ALPHA} --infeasible_alpha ${INFEASIBLE_ALPHA} --num_votes ${NUM_VOTES} --train_steps ${TRAIN_STEPS} --batch_size ${BATCH_SIZE}
+
+echo "Contextual training PRIME BCI EEG 2a high-freq for all subjects/multi-context optimization -> 100k grad. steps."
